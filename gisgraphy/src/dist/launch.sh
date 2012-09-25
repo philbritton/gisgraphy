9e195f0ab0503da943740da6745a8bbe8fa13b30
davidmasclet <davidmasclet@d3fbe6d6-3b18-236d-3785-044ef39e8046>
2012-09-25 05:04:35 +0000

    update launch script

--- gisgraphy/src/dist/launch.sh
+++ gisgraphy/src/dist/launch.sh
@@ -6,4 +6,7 @@ LOG_FILE="./logs/gisgraphy.log"
 # the ammount of memory depends on the amount of data in the fulltext engine. 
 # you can decrease it if you haven't imported a lot of countries
 touch $LOG_FILE
+echo "gisgraphy starting ..."
+echo "Logs are outptut to $LOG_FILE"
+echo "Use the stop script (in the same directory) to shutdown"
 java -DSTOP.PORT=8079 -DSTOP.KEY=stopkey -Dfile.encoding=UTF-8 -Xmx2048m -Xms512m -jar start.jar 2> $LOG_FILE &
