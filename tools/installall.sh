d6bee8d27c3d6a77483c90dd09be2c36d33545b2
davidmasclet <davidmasclet@d3fbe6d6-3b18-236d-3785-044ef39e8046>
2013-04-30 13:10:55 +0000

    add installall script

--- tools/installall.sh
+++ tools/installall.sh
@@ -6,6 +6,13 @@ cd ../universalserialiser
 mvn clean install -Dmaven.test.skip
 cd ../gisgraphy-commons
 mvn clean install -Dmaven.test.skip
+cd ../addressParser-commons
+mvn clean install -Dmaven.test.skip
+if [[ -d "../addressParser-http" ]]
+then
+	cd ../addressParser-http
+	mvn clean install -Dmaven.test.skip
+fi
 cd ../gisgraphy-utils
 mvn clean install -Dmaven.test.skip
 
