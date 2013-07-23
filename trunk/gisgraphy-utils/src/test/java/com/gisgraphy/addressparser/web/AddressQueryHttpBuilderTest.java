675391c8664aa16a3361aa73eb890f946dc9961a
davidmasclet <davidmasclet@d3fbe6d6-3b18-236d-3785-044ef39e8046>
2013-07-23 19:04:57 +0000

    Add the possibility to specify a structured address query

--- trunk/gisgraphy-utils/src/test/java/com/gisgraphy/addressparser/web/AddressQueryHttpBuilderTest.java
+++ trunk/gisgraphy-utils/src/test/java/com/gisgraphy/addressparser/web/AddressQueryHttpBuilderTest.java
@@ -26,12 +26,16 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.commons.lang.RandomStringUtils;
 import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.mock.web.MockHttpServletRequest;
 
+import com.gisgraphy.addressparser.Address;
 import com.gisgraphy.addressparser.AddressQuery;
+import com.gisgraphy.addressparser.StructuredAddressQuery;
 import com.gisgraphy.addressparser.exception.AddressParserException;
 import com.gisgraphy.serializer.common.OutputFormat;
 import com.gisgraphy.servlet.AbstractAddressServlet;
@@ -39,6 +43,27 @@ import com.gisgraphy.servlet.GisgraphyServlet;
 
 
 public class AddressQueryHttpBuilderTest {
+	
+	 @Test
+	    public void buildFromRequestWithNullRequest(){
+	    AddressQueryHttpBuilder builder = AddressQueryHttpBuilder.getInstance();
+	    try {
+			builder.buildAddressFromRequest(null);
+			Assert.fail("could not build a query with null request");
+		} catch (Exception e) {
+		}
+	 }
+	 
+	 
+	 @Test
+	    public void buildQueryFromRequestWithNullRequest(){
+	    AddressQueryHttpBuilder builder = AddressQueryHttpBuilder.getInstance();
+	    try {
+			builder.buildFromRequest(null);
+			Assert.fail("could not build a query with null request");
+		} catch (Exception e) {
+		}
+	 }
     
     @Test
     public void buildFromRequest(){
@@ -84,16 +109,16 @@ public class AddressQueryHttpBuilderTest {
 	}
 	
 	//address
-	//without address
+	//without address nor structured
 	request = new MockHttpServletRequest();
 	request.setParameter(AbstractAddressServlet.COUNTRY_PARAMETER, "US");
 	try {
 		builder.buildFromRequest(request);
-		Assert.fail("without coutry parameter the builder should throws");
+		Assert.fail("without address parameter or structured the builder should throws");
 	} catch (AddressParserException e) {
 		//ignore
 	}
-	//with empty address
+	//with empty address, nor structured
 	request = new MockHttpServletRequest();
 	request.setParameter(AbstractAddressServlet.ADDRESS_PARAMETER, " ");
 	request.setParameter(AbstractAddressServlet.COUNTRY_PARAMETER, "us");
@@ -106,7 +131,7 @@ public class AddressQueryHttpBuilderTest {
 	
 	//with empty country
 	request = new MockHttpServletRequest();
-	request.setParameter(AbstractAddressServlet.ADDRESS_PARAMETER, "");
+	request.setParameter(AbstractAddressServlet.ADDRESS_PARAMETER, " ");
 	request.setParameter(AbstractAddressServlet.COUNTRY_PARAMETER, "us");
 	try {
 		builder.buildFromRequest(request);
@@ -115,7 +140,7 @@ public class AddressQueryHttpBuilderTest {
 		//ignore
 	}
 	
-	//with too long address country
+	//with too long address
 	request = new MockHttpServletRequest();
 	String tooLongAddress = RandomStringUtils.random(AbstractAddressServlet.QUERY_MAX_LENGTH+1);
 	request.setParameter(AbstractAddressServlet.COUNTRY_PARAMETER, "us");
@@ -284,34 +309,278 @@ public class AddressQueryHttpBuilderTest {
 		    + " should be true for 'on' value (case insensitive and on value)  ",
 	    query.isStandardize());
     
+    //callback not set
+    request = new MockHttpServletRequest();
+	request.setParameter(AbstractAddressServlet.ADDRESS_PARAMETER, "address");
+	request.setParameter(AbstractAddressServlet.COUNTRY_PARAMETER, "us");
+	query =builder.buildFromRequest(request);
+    assertNull("callback should be null when not set",
+	     query.getCallback());
+    
+    //callback set with non alpha value
+    request = new MockHttpServletRequest();
+	request.setParameter(AbstractAddressServlet.ADDRESS_PARAMETER, "address");
+	request.setParameter(AbstractAddressServlet.COUNTRY_PARAMETER, "us");
+	request.setParameter(AbstractAddressServlet.CALLBACK_PARAMETER, "doit(");
+	query =builder.buildFromRequest(request);
+    assertNull("callback should not be set when not alphanumeric",
+	     query.getCallback());
+    
+    //callback set with alpha value
+    request = new MockHttpServletRequest();
+	request.setParameter(AbstractAddressServlet.ADDRESS_PARAMETER, "address");
+	request.setParameter(AbstractAddressServlet.COUNTRY_PARAMETER, "us");
+	request.setParameter(AbstractAddressServlet.CALLBACK_PARAMETER, "doit");
+	query =builder.buildFromRequest(request);
+    assertEquals("callback should not be set when alphanumeric",
+	     "doit",query.getCallback());
+	
+    }
+
+    
+    @Test
+    public void buildStructuredFromRequest(){
+    AddressQueryHttpBuilder builder = AddressQueryHttpBuilder.getInstance();
+	MockHttpServletRequest request = new MockHttpServletRequest();
+	//without parameters
+	try {
+		builder.buildFromRequest(request);
+		Assert.fail("without parameters the builder should throws");
+	} catch (AddressParserException e) {
+		//ignore
+	}
+	//country
+	//without country
+	request = new MockHttpServletRequest();
+	request.setParameter(AbstractAddressServlet.ADDRESS_PARAMETER, "address");
+	try {
+		builder.buildFromRequest(request);
+		Assert.fail("without coutry parameter the builder should throws");
+	} catch (AddressParserException e) {
+		//ignore
+	}
+	//with empty country
+	request = new MockHttpServletRequest();
+	request.setParameter(AbstractAddressServlet.COUNTRY_PARAMETER, " ");
+	request.setParameter(AbstractAddressServlet.ADDRESS_PARAMETER, "address");
+	try {
+		builder.buildFromRequest(request);
+		Assert.fail("with empty country equals to space, the builder should throws");
+	} catch (AddressParserException e) {
+		//ignore
+	}
+	
+	//with empty country
+	request = new MockHttpServletRequest();
+	request.setParameter(AbstractAddressServlet.COUNTRY_PARAMETER, "");
+	request.setParameter(AbstractAddressServlet.ADDRESS_PARAMETER, "address");
+	try {
+		builder.buildFromRequest(request);
+		Assert.fail("with empty string the builder should throws");
+	} catch (AddressParserException e) {
+		//ignore
+	}
+	
+	//address
+	//without address
+	request = new MockHttpServletRequest();
+	request.setParameter(AbstractAddressServlet.COUNTRY_PARAMETER, "US");
+	try {
+		builder.buildFromRequest(request);
+		Assert.fail("without address parameter or structured address the builder should throws");
+	} catch (AddressParserException e) {
+		//ignore
+	}
+	//with empty address
+	request = new MockHttpServletRequest();
+	request.setParameter(AbstractAddressServlet.ADDRESS_PARAMETER, " ");
+	request.setParameter(AbstractAddressServlet.COUNTRY_PARAMETER, "us");
+	try {
+		builder.buildFromRequest(request);
+		Assert.fail("with empty country equals to space, the builder should throws");
+	} catch (AddressParserException e) {
+		//ignore
+	}
+	
+	//with empty country
+	request = new MockHttpServletRequest();
+	request.setParameter(AbstractAddressServlet.ADDRESS_PARAMETER, "");
+	request.setParameter(AbstractAddressServlet.COUNTRY_PARAMETER, "us");
+	try {
+		builder.buildFromRequest(request);
+		Assert.fail("with empty string the builder should throws");
+	} catch (AddressParserException e) {
+		//ignore
+	}
+	
+	
+	//all ok
+	request = new MockHttpServletRequest();
+	String city = "city";
+	String houseNumber = "1";
+	String streetName = "california street";
+	request.setParameter(city, city);
+	request.setParameter("houseNumber", houseNumber);
+	request.setParameter("streetName", streetName);
+	request.setParameter("NotExistingFieldName", "foo");
+	request.setParameter("country", "france");
+	request.setParameter(AbstractAddressServlet.COUNTRY_PARAMETER, "us");
+	StructuredAddressQuery structuredquery = (StructuredAddressQuery) builder.buildFromRequest(request);
+	Address address = structuredquery.getStructuredAddress();
+	Assert.assertEquals("us", structuredquery.getCountry());
+	Assert.assertEquals(city, address.getCity());
+	Assert.assertEquals(houseNumber, address.getHouseNumber());
+	Assert.assertEquals(streetName, address.getStreetName());
+	Assert.assertNull(address.getCountry());
+	
+	// test outputFormat
+	// with no value specified
+	request = new MockHttpServletRequest();
+	request.setParameter(AbstractAddressServlet.ADDRESS_PARAMETER, "address");
+	request.setParameter(AbstractAddressServlet.COUNTRY_PARAMETER, "us");
+	AddressQuery query = builder.buildFromRequest(request);
+	assertEquals("When no " + AbstractAddressServlet.OUTPUT_FORMAT_PARAMETER
+		+ " is specified, the  parameter should be set to  "
+		+ OutputFormat.getDefault(), OutputFormat.getDefault(), query
+		.getFormat());
+	// with wrong value
+	request = new MockHttpServletRequest();
+	request.setParameter(AbstractAddressServlet.ADDRESS_PARAMETER, "address");
+	request.setParameter(AbstractAddressServlet.COUNTRY_PARAMETER, "us");
+	request.setParameter(AbstractAddressServlet.OUTPUT_FORMAT_PARAMETER, "UNK");
+	query =builder.buildFromRequest(request);
+	assertEquals("When wrong " + AbstractAddressServlet.OUTPUT_FORMAT_PARAMETER
+		+ " is specified, the  parameter should be set to  "
+		+ OutputFormat.getDefault(), OutputFormat.getDefault(), query
+		.getFormat());
+	// test case sensitive
+	request = new MockHttpServletRequest();
+	request.setParameter(AbstractAddressServlet.ADDRESS_PARAMETER, "address");
+	request.setParameter(AbstractAddressServlet.COUNTRY_PARAMETER, "us");
+	request.setParameter(AbstractAddressServlet.OUTPUT_FORMAT_PARAMETER, "json");
+	query =builder.buildFromRequest(request);
+	assertEquals(AbstractAddressServlet.OUTPUT_FORMAT_PARAMETER
+		+ " should be case insensitive  ", OutputFormat.JSON, query
+		.getFormat());
+	
+	request = new MockHttpServletRequest();
+	request.setParameter(AbstractAddressServlet.ADDRESS_PARAMETER, "address");
+	request.setParameter(AbstractAddressServlet.COUNTRY_PARAMETER, "us");
+	request.setParameter(AbstractAddressServlet.OUTPUT_FORMAT_PARAMETER, "unsupported");
+	query =builder.buildFromRequest(request);
+    assertEquals(GisgraphyServlet.FORMAT_PARAMETER
+	    + " should set default if not supported  ", OutputFormat.getDefault(), query
+	    .getFormat());
+    
+
+    
+    //test indent
+    // with no value specified
+    request = new MockHttpServletRequest();
+	request.setParameter(AbstractAddressServlet.ADDRESS_PARAMETER, "address");
+	request.setParameter(AbstractAddressServlet.COUNTRY_PARAMETER, "us");
+	query =builder.buildFromRequest(request);
+    assertEquals("When no " + AbstractAddressServlet.INDENT_PARAMETER
+	    + " is specified, the  parameter should be set to default indentation",AddressQuery.DEFAULT_INDENTATION
+	    ,query.isIndent());
+    // with wrong value
+    request = new MockHttpServletRequest();
+	request.setParameter(AbstractAddressServlet.ADDRESS_PARAMETER, "address");
+	request.setParameter(AbstractAddressServlet.COUNTRY_PARAMETER, "us");
+	request.setParameter(AbstractAddressServlet.INDENT_PARAMETER, "unk");
+	query =builder.buildFromRequest(request);
+    assertEquals("When wrong " + AbstractAddressServlet.INDENT_PARAMETER
+	    + " is specified, the  parameter should be set to default value",AddressQuery.DEFAULT_INDENTATION,
+	    query.isIndent());
+    // test case sensitive
+    request = new MockHttpServletRequest();
+	request.setParameter(AbstractAddressServlet.ADDRESS_PARAMETER, "address");
+	request.setParameter(AbstractAddressServlet.COUNTRY_PARAMETER, "us");
+	request.setParameter(AbstractAddressServlet.INDENT_PARAMETER, "TrUe");
+	query =builder.buildFromRequest(request);
+    assertTrue(AbstractAddressServlet.INDENT_PARAMETER
+	    + " should be case insensitive  ", query.isIndent());
+    // test 'on' value
+    request = new MockHttpServletRequest();
+	request.setParameter(AbstractAddressServlet.ADDRESS_PARAMETER, "address");
+	request.setParameter(AbstractAddressServlet.COUNTRY_PARAMETER, "us");
+	request.setParameter(AbstractAddressServlet.INDENT_PARAMETER, "On");
+	query =builder.buildFromRequest(request);
+    assertTrue(
+    		AbstractAddressServlet.INDENT_PARAMETER
+		    + " should be true for 'on' value (case insensitive and on value)  ",
+	    query.isIndent());
+    
+    //test postal
+    // with no value specified
+    request = new MockHttpServletRequest();
+	request.setParameter(AbstractAddressServlet.ADDRESS_PARAMETER, "address");
+	request.setParameter(AbstractAddressServlet.COUNTRY_PARAMETER, "us");
+	query =builder.buildFromRequest(request);
+    assertFalse("When no " + AbstractAddressServlet.POSTAL_PARAMETER
+	    + " is specified, the  parameter should be set to false",query.isPostal());
+    // with wrong value
+    request = new MockHttpServletRequest();
+	request.setParameter(AbstractAddressServlet.ADDRESS_PARAMETER, "address");
+	request.setParameter(AbstractAddressServlet.COUNTRY_PARAMETER, "us");
+	request.setParameter(AbstractAddressServlet.POSTAL_PARAMETER, "unk");
+	query =builder.buildFromRequest(request);
+    assertFalse("When wrong " + AbstractAddressServlet.POSTAL_PARAMETER
+	    + " is specified, the  parameter should be set to false", query.isPostal());
+    // test case sensitive
+    request = new MockHttpServletRequest();
+	request.setParameter(AbstractAddressServlet.ADDRESS_PARAMETER, "address");
+	request.setParameter(AbstractAddressServlet.COUNTRY_PARAMETER, "us");
+	request.setParameter(AbstractAddressServlet.POSTAL_PARAMETER, "TrUe");
+	query =builder.buildFromRequest(request);
+    assertTrue(AbstractAddressServlet.POSTAL_PARAMETER
+	    + " should be case insensitive  ", query.isPostal());
+    // test 'on' value
+    request = new MockHttpServletRequest();
+	request.setParameter(AbstractAddressServlet.ADDRESS_PARAMETER, "address");
+	request.setParameter(AbstractAddressServlet.COUNTRY_PARAMETER, "us");
+	request.setParameter(AbstractAddressServlet.POSTAL_PARAMETER, "On");
+	query =builder.buildFromRequest(request);
+    assertTrue(
+    		AbstractAddressServlet.POSTAL_PARAMETER
+		    + " should be true for 'on' value (case insensitive and on value)  ",
+	    query.isPostal());
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    //test standardize
+    // with no value specified
+    request = new MockHttpServletRequest();
+	request.setParameter(AbstractAddressServlet.ADDRESS_PARAMETER, "address");
+	request.setParameter(AbstractAddressServlet.COUNTRY_PARAMETER, "us");
+	query =builder.buildFromRequest(request);
+    assertFalse("When no " + AbstractAddressServlet.STANDARDIZE_PARAMETER
+	    + " is specified, the  parameter should be set to false",query.isStandardize());
+    // with wrong value
+    request = new MockHttpServletRequest();
+	request.setParameter(AbstractAddressServlet.ADDRESS_PARAMETER, "address");
+	request.setParameter(AbstractAddressServlet.COUNTRY_PARAMETER, "us");
+	request.setParameter(AbstractAddressServlet.STANDARDIZE_PARAMETER, "unk");
+	query =builder.buildFromRequest(request);
+    assertFalse("When wrong " + AbstractAddressServlet.STANDARDIZE_PARAMETER
+	    + " is specified, the  parameter should be set to false", query.isStandardize());
+    // test case sensitive
+    request = new MockHttpServletRequest();
+	request.setParameter(AbstractAddressServlet.ADDRESS_PARAMETER, "address");
+	request.setParameter(AbstractAddressServlet.COUNTRY_PARAMETER, "us");
+	request.setParameter(AbstractAddressServlet.STANDARDIZE_PARAMETER, "TrUe");
+	query =builder.buildFromRequest(request);
+    assertTrue(AbstractAddressServlet.STANDARDIZE_PARAMETER
+	    + " should be case insensitive  ", query.isStandardize());
+    // test 'on' value
+    request = new MockHttpServletRequest();
+	request.setParameter(AbstractAddressServlet.ADDRESS_PARAMETER, "address");
+	request.setParameter(AbstractAddressServlet.COUNTRY_PARAMETER, "us");
+	request.setParameter(AbstractAddressServlet.STANDARDIZE_PARAMETER, "On");
+	query =builder.buildFromRequest(request);
+    assertTrue(
+    		AbstractAddressServlet.STANDARDIZE_PARAMETER
+		    + " should be true for 'on' value (case insensitive and on value)  ",
+	    query.isStandardize());
     
     //callback not set
     request = new MockHttpServletRequest();
@@ -339,6 +608,44 @@ public class AddressQueryHttpBuilderTest {
     assertEquals("callback should not be set when alphanumeric",
 	     "doit",query.getCallback());
 	
+    }
+    
+    
+    @Test
+    public void buildAddressFromRequest(){
+    	AddressQueryHttpBuilder builder = AddressQueryHttpBuilder.getInstance();
+    	MockHttpServletRequest request = new MockHttpServletRequest();
+    	String city = "city";
+    	String houseNumber = "1";
+    	String streetName = "california street";
+		request.setParameter(city, city);
+		request.setParameter("houseNumber", houseNumber);
+		request.setParameter("streetName", streetName);
+		request.setParameter("NotExistingFieldName", "foo");
+		request.setParameter("country", "france");
+    	Address address = builder.buildAddressFromRequest(request);
+    	Assert.assertEquals(city, address.getCity());
+    	Assert.assertEquals(houseNumber, address.getHouseNumber());
+    	Assert.assertEquals(streetName, address.getStreetName());
+    	Assert.assertNull(address.getCountry());
+    }
+    
+    @Test
+    public void buildAddressFromRequest_WithoutParameters(){
+    	AddressQueryHttpBuilder builder = AddressQueryHttpBuilder.getInstance();
+    	MockHttpServletRequest request = new MockHttpServletRequest();
+		request.setParameter("NotExistingFieldName", "foo");
+    	Address address = builder.buildAddressFromRequest(request);
+    	Assert.assertNull(address);
+    }
+    
+    @Test
+    public void buildAddressFromRequest_WithOnlyCountryParameters(){
+    	AddressQueryHttpBuilder builder = AddressQueryHttpBuilder.getInstance();
+    	MockHttpServletRequest request = new MockHttpServletRequest();
+		request.setParameter("country", "foo");
+    	Address address = builder.buildAddressFromRequest(request);
+    	Assert.assertNull("country parameter is not really an address field, it is often the country parameter to geocode",address);
     }
 
 }
