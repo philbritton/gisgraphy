b22e8c98a7acb100598c9e753987cdcd0b9a3e86
davidmasclet <davidmasclet@d3fbe6d6-3b18-236d-3785-044ef39e8046>
2013-10-29 22:04:28 +0000

    improve osm cities importer

--- gisgraphy/src/test/java/com/gisgraphy/importer/OpenStreetMapCitiesSimpleImporterTest.java
+++ gisgraphy/src/test/java/com/gisgraphy/importer/OpenStreetMapCitiesSimpleImporterTest.java
@@ -1,13 +1,312 @@
 package com.gisgraphy.importer;
 
-import static org.junit.Assert.*;
+import static com.gisgraphy.fulltext.Constants.ONLY_CITY_PLACETYPE;
+import static com.gisgraphy.importer.OpenStreetMapCitiesSimpleImporter.MINIMUM_OUTPUT_STYLE;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.easymock.EasyMock;
+import org.junit.Assert;
 import org.junit.Test;
+
+import com.gisgraphy.domain.geoloc.entity.Adm;
+import com.gisgraphy.domain.geoloc.entity.City;
+import com.gisgraphy.domain.repository.IAdmDao;
+import com.gisgraphy.domain.repository.ICityDao;
+import com.gisgraphy.domain.repository.IIdGenerator;
+import com.gisgraphy.domain.valueobject.GISSource;
+import com.gisgraphy.domain.valueobject.Pagination;
+import com.gisgraphy.fulltext.Constants;
+import com.gisgraphy.fulltext.FulltextQuery;
+import com.gisgraphy.fulltext.FulltextResultsDto;
+import com.gisgraphy.fulltext.IFullTextSearchEngine;
+import com.gisgraphy.fulltext.SolrResponseDto;
+import com.gisgraphy.helper.GeolocHelper;
+import com.vividsolutions.jts.geom.Point;
 
 public class OpenStreetMapCitiesSimpleImporterTest {
 
 	@Test
-	public void test() {
+	public void createNewCity() {
+		OpenStreetMapCitiesSimpleImporter importer = new OpenStreetMapCitiesSimpleImporter();
+		IIdGenerator idGenerator = EasyMock.createMock(IIdGenerator.class);
+    	EasyMock.expect(idGenerator.getNextFeatureId()).andReturn(1234L);
+    	EasyMock.replay(idGenerator);
+    	importer.setIdGenerator(idGenerator);
+		importer.idGenerator= idGenerator;
+		City actual = importer.createNewCity();
+		Assert.assertEquals(GISSource.OPENSTREETMAP, actual.getSource());
+		Assert.assertEquals(1234L, actual.getFeatureId().longValue());
 	}
 
+	@Test
+	public void getNearestCity(){
+		OpenStreetMapCitiesSimpleImporter importer = new OpenStreetMapCitiesSimpleImporter();
+		
+		List<SolrResponseDto> results = new ArrayList<SolrResponseDto>();
+		SolrResponseDto solrResponseDto = EasyMock.createNiceMock(SolrResponseDto.class);
+		results.add(solrResponseDto);
+		FulltextResultsDto mockResultDTO = EasyMock.createMock(FulltextResultsDto.class);
+		EasyMock.expect(mockResultDTO.getResultsSize()).andReturn(1);
+		EasyMock.expect(mockResultDTO.getResults()).andReturn(results);
+		EasyMock.replay(mockResultDTO);
+		
+		
+		String text = "toto";
+		String countryCode = "FR";
+		Point location = GeolocHelper.createPoint(3F, 4F);
+		IFullTextSearchEngine mockfullFullTextSearchEngine = EasyMock.createMock(IFullTextSearchEngine.class);
+		FulltextQuery query = new FulltextQuery(text, Pagination.ONE_RESULT, OpenStreetMapCitiesSimpleImporter.MINIMUM_OUTPUT_STYLE, ONLY_CITY_PLACETYPE, countryCode);
+		query.withAllWordsRequired(false).withoutSpellChecking();
+		
+		EasyMock.expect(mockfullFullTextSearchEngine.executeQuery(query)).andReturn(mockResultDTO);
+		EasyMock.replay(mockfullFullTextSearchEngine);
+		
+		importer.setFullTextSearchEngine(mockfullFullTextSearchEngine);
+		
+		SolrResponseDto actual = importer.getNearestCity(location, text, countryCode);
+		Assert.assertEquals(solrResponseDto, actual);
+		EasyMock.verify(mockfullFullTextSearchEngine);
+	}
+	
+	@Test
+	public void getNearestCityWithNullName(){
+		OpenStreetMapCitiesSimpleImporter importer = new OpenStreetMapCitiesSimpleImporter();
+		Point location = GeolocHelper.createPoint(3F, 4F);
+		Assert.assertNull(importer.getNearestCity(location, "", "FR"));
+		Assert.assertNull(importer.getNearestCity(location, " ", "FR"));
+		Assert.assertNull(importer.getNearestCity(location, null, "FR"));
+	}
+	
+	@Test
+	public void getNearestCityWithNullLocation(){
+		OpenStreetMapCitiesSimpleImporter importer = new OpenStreetMapCitiesSimpleImporter();
+		Assert.assertNull(importer.getNearestCity(null, "paris", "FR"));
+		
+	}
+	
+	@Test
+	public void getAdm(){
+		OpenStreetMapCitiesSimpleImporter importer = new OpenStreetMapCitiesSimpleImporter();
+		
+		List<SolrResponseDto> results = new ArrayList<SolrResponseDto>();
+		SolrResponseDto solrResponseDto = EasyMock.createNiceMock(SolrResponseDto.class);
+		results.add(solrResponseDto);
+		FulltextResultsDto mockResultDTO = EasyMock.createMock(FulltextResultsDto.class);
+		EasyMock.expect(mockResultDTO.getResultsSize()).andReturn(1);
+		EasyMock.expect(mockResultDTO.getResults()).andReturn(results);
+		EasyMock.replay(mockResultDTO);
+		
+		
+		String text = "toto";
+		String countryCode = "FR";
+		IFullTextSearchEngine mockfullFullTextSearchEngine = EasyMock.createMock(IFullTextSearchEngine.class);
+		FulltextQuery query = new FulltextQuery(text, Pagination.ONE_RESULT, MINIMUM_OUTPUT_STYLE, Constants.ONLY_ADM_PLACETYPE, countryCode);
+		query.withAllWordsRequired(false).withoutSpellChecking();
+		
+		EasyMock.expect(mockfullFullTextSearchEngine.executeQuery(query)).andReturn(mockResultDTO);
+		EasyMock.replay(mockfullFullTextSearchEngine);
+		
+		importer.setFullTextSearchEngine(mockfullFullTextSearchEngine);
+		
+		SolrResponseDto actual = importer.getAdm(text, countryCode);
+		Assert.assertEquals(solrResponseDto, actual);
+		
+		EasyMock.verify(mockfullFullTextSearchEngine);
+		
+	}
+	
+	@Test
+	public void processWithUnknownCityAndKnownAdm(){
+		
+		final SolrResponseDto solrResponseDtoAdm = EasyMock.createMock(SolrResponseDto.class);
+		EasyMock.expect(solrResponseDtoAdm.getFeature_id()).andReturn(4356L);
+		EasyMock.replay(solrResponseDtoAdm);
+		OpenStreetMapCitiesSimpleImporter importer = new OpenStreetMapCitiesSimpleImporter(){
+			protected SolrResponseDto getNearestCity(Point location, String name, String countryCode) {
+				if (!name.equals("paris") || !countryCode.equals("FR")){
+					throw new RuntimeException("the function is not called with the correct parameter");
+				}
+				return null;
+			};
+			
+			@Override
+			protected SolrResponseDto getAdm(String name, String countryCode) {
+				if (!name.equals("Europe") || !countryCode.equals("FR")){
+					throw new RuntimeException("the function is not called with the correct parameter");
+				}
+				return solrResponseDtoAdm;
+			}
+		};
+		
+		ICityDao cityDao = EasyMock.createMock(ICityDao.class);
+		City city=new City();
+		city.setFeatureId(9876L);
+		EasyMock.expect(cityDao.save(city)).andReturn(city);
+		EasyMock.replay(cityDao);
+		importer.setCityDao(cityDao);
+		
+		IIdGenerator idGenerator = EasyMock.createMock(IIdGenerator.class);
+		EasyMock.expect(idGenerator.getNextFeatureId()).andReturn(9876L);
+		EasyMock.replay(idGenerator);
+		importer.setIdGenerator(idGenerator);
+		
+		IAdmDao admDao = EasyMock.createMock(IAdmDao.class);
+		Adm adm = new Adm(2);
+		EasyMock.expect(admDao.getByFeatureId(4356L)).andReturn(adm);
+		EasyMock.replay(admDao);
+		importer.setAdmDao(admDao);
+		
+		
+		String line= "N\t1234\tparis\tFR\t5678\t1000000\t0101000020E61000004070F0E0825F30405F65C80CAF1A4840\t\tcity\tEurope";
+		
+		importer.processData(line);
+		
+		EasyMock.verify(cityDao);
+		EasyMock.verify(admDao);
+		EasyMock.verify(idGenerator);
+	}
+	
+	@Test
+	public void processWithknownCityAndAdm(){
+		final SolrResponseDto solrResponseDtoCity = EasyMock.createMock(SolrResponseDto.class);
+		EasyMock.expect(solrResponseDtoCity.getFeature_id()).andReturn(123L);
+		EasyMock.replay(solrResponseDtoCity);
+		
+		final SolrResponseDto solrResponseDtoAdm = EasyMock.createMock(SolrResponseDto.class);
+		EasyMock.expect(solrResponseDtoAdm.getFeature_id()).andReturn(4356L);
+		EasyMock.replay(solrResponseDtoAdm);
+		OpenStreetMapCitiesSimpleImporter importer = new OpenStreetMapCitiesSimpleImporter(){
+			protected SolrResponseDto getNearestCity(Point location, String name, String countryCode) {
+				if (!name.equals("paris") || !countryCode.equals("FR")){
+					throw new RuntimeException("the function is not called with the correct parameter");
+				}
+				return solrResponseDtoCity;
+			};
+			
+			@Override
+			protected SolrResponseDto getAdm(String name, String countryCode) {
+				if (!name.equals("Europe") || !countryCode.equals("FR")){
+					throw new RuntimeException("the function is not called with the correct parameter");
+				}
+				return solrResponseDtoAdm;
+			}
+		};
+		
+		ICityDao cityDao = EasyMock.createMock(ICityDao.class);
+		City city=new City();
+		city.setFeatureId(123L);
+		EasyMock.expect(cityDao.getByFeatureId(123L)).andReturn(city);
+		EasyMock.expect(cityDao.save(city)).andReturn(city);
+		EasyMock.replay(cityDao);
+		importer.setCityDao(cityDao);
+		
+		
+		IAdmDao admDao = EasyMock.createMock(IAdmDao.class);
+		Adm adm = new Adm(2);
+		EasyMock.expect(admDao.getByFeatureId(4356L)).andReturn(adm);
+		EasyMock.replay(admDao);
+		importer.setAdmDao(admDao);
+		
+		
+		String line= "N\t1234\tparis\tFR\t5678\t1000000\t0101000020E61000004070F0E0825F30405F65C80CAF1A4840\t\tcity\tEurope";
+		
+		importer.processData(line);
+		
+		EasyMock.verify(cityDao);
+		EasyMock.verify(admDao);
+	}
+	
+	@Test
+	public void processWithunKnownCityAndUnknownAdm(){
+		
+		OpenStreetMapCitiesSimpleImporter importer = new OpenStreetMapCitiesSimpleImporter(){
+			protected SolrResponseDto getNearestCity(Point location, String name, String countryCode) {
+				if (!name.equals("paris") || !countryCode.equals("FR")){
+					throw new RuntimeException("the function is not called with the correct parameter");
+				}
+				return null;
+			};
+			
+			@Override
+			protected SolrResponseDto getAdm(String name, String countryCode) {
+				if (!name.equals("Europe") || !countryCode.equals("FR")){
+					throw new RuntimeException("the function is not called with the correct parameter");
+				}
+				return null;
+			}
+		};
+		
+		ICityDao cityDao = EasyMock.createMock(ICityDao.class);
+		City city=new City();
+		city.setFeatureId(9876L);
+		EasyMock.expect(cityDao.save(city)).andReturn(city);
+		EasyMock.replay(cityDao);
+		importer.setCityDao(cityDao);
+		
+		IIdGenerator idGenerator = EasyMock.createMock(IIdGenerator.class);
+		EasyMock.expect(idGenerator.getNextFeatureId()).andReturn(9876L);
+		EasyMock.replay(idGenerator);
+		importer.setIdGenerator(idGenerator);
+		
+		
+		IAdmDao admDao = EasyMock.createMock(IAdmDao.class);
+		EasyMock.replay(admDao);
+		importer.setAdmDao(admDao);
+		
+		
+		String line= "N\t1234\tparis\tFR\t5678\t1000000\t0101000020E61000004070F0E0825F30405F65C80CAF1A4840\t\tcity\tEurope";
+		
+		importer.processData(line);
+		
+		EasyMock.verify(cityDao);
+		EasyMock.verify(admDao);
+		EasyMock.verify(idGenerator);
+	}
+	
+	@Test
+	public void processWithKnownCityAndUnknownAdm(){
+		final SolrResponseDto solrResponseDtoCity = EasyMock.createMock(SolrResponseDto.class);
+		EasyMock.expect(solrResponseDtoCity.getFeature_id()).andReturn(123L);
+		EasyMock.replay(solrResponseDtoCity);
+		
+		OpenStreetMapCitiesSimpleImporter importer = new OpenStreetMapCitiesSimpleImporter(){
+			protected SolrResponseDto getNearestCity(Point location, String name, String countryCode) {
+				if (!name.equals("paris") || !countryCode.equals("FR")){
+					throw new RuntimeException("the function is not called with the correct parameter");
+				}
+				return solrResponseDtoCity;
+			};
+			
+			@Override
+			protected SolrResponseDto getAdm(String name, String countryCode) {
+				if (!name.equals("Europe") || !countryCode.equals("FR")){
+					throw new RuntimeException("the function is not called with the correct parameter");
+				}
+				return null;
+			}
+		};
+		
+		ICityDao cityDao = EasyMock.createMock(ICityDao.class);
+		City city=new City();
+		city.setFeatureId(123L);
+		EasyMock.expect(cityDao.getByFeatureId(123L)).andReturn(city);
+		EasyMock.expect(cityDao.save(city)).andReturn(city);
+		EasyMock.replay(cityDao);
+		importer.setCityDao(cityDao);
+		
+		
+		IAdmDao admDao = EasyMock.createMock(IAdmDao.class);
+		EasyMock.replay(admDao);
+		importer.setAdmDao(admDao);
+		
+		
+		String line= "N\t1234\tparis\tFR\t5678\t1000000\t0101000020E61000004070F0E0825F30405F65C80CAF1A4840\t\tcity\tEurope";
+		
+		importer.processData(line);
+		
+		EasyMock.verify(cityDao);
+		EasyMock.verify(admDao);
+	}
 }
