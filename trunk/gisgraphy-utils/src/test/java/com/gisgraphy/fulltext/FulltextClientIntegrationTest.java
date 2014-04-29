d8455a9f84bdcdb40f68dc0a30229c7323dcb571
davidmasclet <davidmasclet@d3fbe6d6-3b18-236d-3785-044ef39e8046>
2014-04-29 11:11:26 +0000

    add a draft for relevance tests

--- trunk/gisgraphy-utils/src/test/java/com/gisgraphy/fulltext/FulltextClientIntegrationTest.java
+++ trunk/gisgraphy-utils/src/test/java/com/gisgraphy/fulltext/FulltextClientIntegrationTest.java
@@ -14,7 +14,7 @@ import com.gisgraphy.test.GisgraphyUtilsTestHelper;
 
 public class FulltextClientIntegrationTest {
 
-	private static final String BASE_URL = "http://localhost:8080/fulltext/";
+	private static final String BASE_URL = GisgraphyUtilsTestHelper.HTTP_BASE_URL+"/fulltext/";
 
 	@Test
 	public void executeQueryCountry() {
