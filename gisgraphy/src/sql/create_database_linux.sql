f728ad47529779f1ad272e4aa477851853f8cd1e
davidmasclet <davidmasclet@d3fbe6d6-3b18-236d-3785-044ef39e8046>
2014-07-09 19:43:30 +0000

    update site fix bug 38

--- gisgraphy/src/sql/create_database_linux.sql
+++ gisgraphy/src/sql/create_database_linux.sql
@@ -1,4 +1,4 @@
--- edit user/password/database if needed. The postgis path depends on your linux distribution
+-- edit user/password/database if needed. The postgis path depends on your linux distribution and postgres/postgis version and should be probably modified
  
 psql -U postgres  -h 127.0.0.1 -c  "CREATE DATABASE gisgraphy ENCODING = 'UTF8';"
 createlang -U postgres -h 127.0.0.1 plpgsql gisgraphy 
